54e893abfd82599d1d455d4c9b543dfa020d2712
Seymur Bagirov <seimur313@gmail.com>
2021-08-12 01:56:27 +0400

    added homework

---
+++ c_strings.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+#include <cmath>
+
+auto StringToNumber(char* str)
+{
+    // long long number = 0;
+    // int j = strlen(str) - 1;
+    // for (int i = 0; i < strlen(str); i++)
+    // {
+    //     long long temp = std::pow(10, j--);
+    //     int num = (str[i] + 2) - 50; // 0 - 48, (48 + 2) - 50 = 0; 1 - 49, (49 + 2) - 50 = 1;
+    //     number += (temp * num);
+    // }
+    // return number;
+    long long number = 0;
+    long long ten = 1;
+    for (int i = strlen(str) - 1; i >= 0; i--)
+    {
+        int num = (str[i] + 2) - 50; // 0 - 48, (48 + 2) - 50 = 0; 1 - 49, (49 + 2) - 50 = 1;
+        number += (ten * num);
+        ten *= 10;
+    }
+    return number;
+}
+
+char* NumberToString(int number)
+{
+    
+    while(number != 0)
+    {
+        int num = number % 10;
+        number /= 10;
+        
+    }
+}
+
+
+int main()
+{
+    char* number = new char[30];
+    std::cin.getline(number, 30);
+    std::cout << StringToNumber(number);
+}
