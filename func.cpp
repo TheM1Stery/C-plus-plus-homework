f3d3871f27ea7b5bcf55b21e1f10e071dea42642
Seymur Bagirov <megadestroyer230@gmail.com>
2021-08-12 21:54:18 +0400

    Finished func.cpp

--- func.cpp
+++ func.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 
 
-template <typename T> int find(T* arr, const int size)
+template <typename T> size_t find(T* arr, T item, const int size)
 {
-	return;
-}
 
-
-template <typename T>void reverse(T* arr, const int size)
-{
-	for (int i = 0; i < size / 2; i++)
+	for (int i = 0; i < size; i++)
 	{
-		std::swap(*(arr + i), *(arr + size - i - 1));
+		if (item == arr[i])
+		{
+			return i;
+		}
 	}
-		
+	return -1;
 }
 
-template <typename T> T* remove(T* arr, int remove,  int* const size)
+
+template <typename T> void remove(T*& arr, T remove,  size_t& size)
 {
-	T* new_arr = new int[*size - 1];
-	int c = 0;
+	
 	int index = -1;
-	for (int i = 0; i < *size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (arr[i] == remove)
 		{
@@ -31,9 +29,11 @@ template <typename T> T* remove(T* arr, int remove,  int* const size)
 	}
 	if (index == -1)
 	{
-		return arr;
+		return;
 	}
-	for (int i = 0; i < *size; i++)
+	T* new_arr = new int[size - 1];
+	int c = 0;
+	for (int i = 0; i < size; i++)
 	{
 		if (i != index)
 		{
@@ -42,16 +42,17 @@ template <typename T> T* remove(T* arr, int remove,  int* const size)
 		}
 
 	}
-	(*size)--;
-	return new_arr;
+	size--;
+	delete[] arr;
+	arr = new_arr;
 }
 
 
-template <typename T> T* insert(T* arr, int insert,int index, int* const size)
+template <typename T> void insert(T*& arr, T index,T insert, size_t& size)
 {
-	T* new_arr = new int[*size + 1];
+	T* new_arr = new T[size + 1];
 	int c = 0;
-	for (int i = 0; i < *size; i++)
+	for (int i = 0; i < size + 1; i++)
 	{
 		if (i != index)
 		{
@@ -60,18 +61,23 @@ template <typename T> T* insert(T* arr, int insert,int index, int* const size)
 		}else
 		{
 			new_arr[c++] = insert;
-			new_arr[c++] = arr[i];
+			if (i < size)
+			{
+				new_arr[c++] = arr[i];
+			}
+			
 		}
 	}
-	(*size)++;
-	return new_arr;
+	size++;
+	delete[] arr;
+	arr = new_arr;
 }
 
-template <typename T> T* shift(T* arr, int* const size)
+template <typename T> void shift(T*& arr, size_t& size)
 {
-	T* new_arr = new int[*size - 1];
+	T* new_arr = new int[size - 1];
 	int c = 0;
-	for (int i = 0; i < *size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (i != 0)
 		{
@@ -80,39 +86,40 @@ template <typename T> T* shift(T* arr, int* const size)
 		}
 
 	}
-	(*size)--;
-	return new_arr;
+	size--;
+	delete[] arr;
+	arr = new_arr;
 }
 
 
-template <typename T> T* pop(T* arr, int* const size)
+template <typename T> void pop(T*& arr, size_t& size)
 {
-	T* new_arr = new int[*size - 1];
+	T* new_arr = new int[size - 1];
 	int c = 0;
-	for (int i = 0; i < *size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		if (i != *size - 1)
+		if (i != size - 1)
 		{
 			new_arr[c] = arr[i];
 			c++;
 		}
 
 	}
-	(*size)--;
-	return new_arr;
+	size--;
+	delete[] arr;
+	arr = new_arr;
 }
 
 
-template <typename T> T* pop(T* arr, int index, int* const size)
+template <typename T> void pop(T*& arr, int index, size_t& size)
 {
-	
-	T* new_arr = new int[*size - 1];
-	if (index >= *size || index < 0)
+	if (index >= size || index < 0)
 	{
-		return arr;
+		return;
 	}
+	T* new_arr = new int[size - 1];
 	int c = 0;
-	for (int i = 0; i < *size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		if (i != index)
 		{
@@ -121,56 +128,68 @@ template <typename T> T* pop(T* arr, int index, int* const size)
 		}
 		
 	}
-	(*size)--;
-	return new_arr;
+	size--;
+	delete[] arr;
+	arr = new_arr;
 }
 
-template <typename T> T* append(T* arr, int append, int* const size)
+template <typename T> void append(T*& arr, T append, size_t& size)
 {
-	(*size)++;
-	T* r_arr = new int[*size];
-	r_arr[*size - 1] = append;
-	for (int i = 0; i < *size - 1; i++)
+	size++;
+	T* r_arr = new int[size];
+	r_arr[size - 1] = append;
+	for (int i = 0; i < size - 1; i++)
 	{
 		r_arr[i] = arr[i];
 	}
-	return r_arr;
+	delete[] arr;
+	arr = r_arr;
 }
 
-template <typename T> T* prepend(T* arr,int prepend, int* const size)
+template <typename T> void prepend(T*& arr,T prepend, size_t& size)
 {
-	(*size)++;
-	T* r_arr = new int[*size];
+	size++;
+	T* r_arr = new int[size];
 	r_arr[0] = prepend;
-	for (int i = 1; i < *size; i++)
+	for (int i = 1; i < size; i++)
 	{
 		r_arr[i] = arr[i - 1];
 	}
-	return r_arr;
+	delete[] arr;
+	arr = r_arr;
 }
 
-template <typename T> T* extend(T* arr, T* arr2, int* const size1, const int size2)
+template <typename T> T* extend(T*& arr, T* arr2, size_t& size1, const size_t size2)
 {
 	
-	T* r_arr = new int[*size1 + size2];
+	T* r_arr = new int[size1 + size2];
 	int c = 0;
-	for (int i = 0; i < *size1 + size2; i++)
+	for (int i = 0; i < size1 + size2; i++)
 	{
-		if (i < *size1)
+		if (i < size1)
 		{
 			r_arr[i] = arr[i];
 		}
-		if (i >= *size1)
+		if (i >= size1)
 		{
 			r_arr[i] = arr2[c++];
 		}
 	}
-	*size1 = *size1 + size2;
-	return r_arr;
+	size1 += size2;
+	delete[] arr;
+	arr = r_arr;
 }
 
+template <typename T>void reverse(T*& arr, size_t& size)
+{
+	for (int i = 0; i < size / 2; i++)
+	{
+		std::swap(*(arr + i), *(arr + size - i - 1));
+	}
+		
+}
 
-template <typename T> void print_array(T arr[], const int size)
+template <typename T> void print_array(T* arr, const int size)
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -181,11 +200,11 @@ template <typename T> void print_array(T arr[], const int size)
 
 int main()
 {
-	int* arr = new int[10] { 1,2,3,4,5,6,7,8,9,10 };
-	int* arr2 = new int[6] { 11,12,13,14,15, 16};
-	int size = 10;
-	int size2 = 6;
+	size_t size = 10;
+	int* arr = new int[size] { 1,2,3,4,5,6,7,8,9,10 };
+	// int* arr2 = new int[6] { 11,12,13,14,15, 16};
 	print_array(arr, size);
-	arr = insert(arr, 99, 3, &size);
+	append(arr, 10, size);
 	print_array(arr, size);
+
 }
