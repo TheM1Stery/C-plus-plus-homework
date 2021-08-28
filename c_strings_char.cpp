a50df3e43804a2bfbedace7d2f43c323627f47cd
Seymur Bagirov <megadestroyer230@gmail.com>
2021-08-29 02:54:54 +0400

    add c_string_char.cpp

---
+++ c_strings_char.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string.h>
+
+
+char* pop(char* str, int number)
+{
+   
+    const size_t length = strlen(str);
+    if (number > length)
+    {
+        return str;
+    }
+    int index = number - 1;
+    char* temp = &str[index];
+    int i = 0;
+    while(temp[i] != '\0')
+    {
+        temp[i] = temp[i + 1];
+        i++;
+    }
+    // memmove(temp, temp + 1, length); // memcpy(temp, temp + 1, length)
+    return str;
+}
+
+char* remove_all(char* str, char symbol)
+{
+    const size_t length = strlen(str);
+    char* temp = strchr(str, symbol);
+    if (temp == nullptr)
+    {
+        return str;
+    }
+    while(temp != nullptr)
+    {
+        int i = 0;
+        while(temp[i] != '\0')
+        {
+            temp[i] = temp[i + 1];
+            i++;
+        }
+        temp = strchr(temp, symbol);
+    }
+    return str;
+}
+
+char* insert(char*& str, int pos,char symbol,  size_t buffer_size)
+{
+    size_t length = strlen(str);
+    if (buffer_size < length + 1)
+    {
+        char* new_str = (char*) operator new[] (length + 100);
+        memcpy(new_str, str, length + 1);
+        delete[] str;
+        str = new_str;
+    }
+    char* temp  = &str[pos];
+    int move = temp[0];
+    temp[0] = symbol;
+    int i = 1;
+    while(i < length + 1)
+    {
+        int move_next = move;
+        move = temp[i];
+        temp[i] = move_next;
+        i++;
+    }
+    return str;
+}
+
+int main()
+{
+    char* str = new char[100];
+    std::cin.getline(str, 100);
+    // std::cout << pop(str, 1) << std::endl;
+    std::cout << insert(str, 2, 's', 100);
+}
