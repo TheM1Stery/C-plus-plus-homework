f543e2f5fa0b9764ca864a389132e7caec132581
Seymur Bagirov <megadestroyer230@gmail.com>
2021-09-16 02:10:00 +0400

    add fraction operations

--- structs.cpp
+++ structs.cpp
@@ -16,6 +16,7 @@ struct Axis
 
 struct Fraction
 {
+	int mix{};
 	int numerator;
 	int denominator;
 };
@@ -24,6 +25,8 @@ struct Fraction
 int greatest_common_divider(int a, int b)
 {
 	int remainder;
+	a = abs(a);
+	b = abs(b);
 	while (a % b > 0)
 	{
 		remainder = a % b;
@@ -34,30 +37,85 @@ int greatest_common_divider(int a, int b)
 	
 }
 
+int least_common_divisor(int a, int b)
+{
+	return abs(a * b) / greatest_common_divider(a, b);
+}
+
+
+
 
 void print_fraction(Fraction* fraction)
+{
+	if (fraction->mix != 0)
+	{
+		std::cout << fraction->mix << " " << fraction->numerator << "|" << fraction->denominator;
+	}
+	if (fraction->mix == 0)
+	{
+		std::cout << fraction->numerator << "|" << fraction->denominator;
+	}
+	
+}
+
+
+void reduce(Fraction* fraction)
+{
+	int c = greatest_common_divider(fraction->numerator, fraction->denominator);
+	if (c > 1)
+	{
+		fraction->numerator /= c;
+		fraction->denominator /= c;
+	}
+}
+
+void convert_to_mix(Fraction* fraction)
 {
 	// 12 / 5 - 2 
 	int number{};
 	if (fraction->numerator > fraction->denominator)
 	{
 		number = fraction->numerator % fraction->denominator;
+		fraction->numerator -= (fraction->denominator * number);
 	}
-	int c = greatest_common_divider(fraction->numerator, fraction->denominator);
-	if (number > 0)
-	{
-		std::cout << number;
-	}
-
-	std::cout << fraction->numerator << " \\ " << fraction->denominator;
-	
+	fraction->mix = number;
 }
 
+Fraction* sum_fractions(Fraction* fraction1, Fraction* fraction2) // было бы прикольно оверлоднуть оператор +, чтобы он делал то что я щас делаю в функции
+{
+	Fraction* fraction_result = new Fraction();
+	int lcm = least_common_divisor(fraction1->denominator, fraction2->denominator);
+	int a = fraction1->numerator;
+	int b = fraction2->numerator;
+	if (fraction1->denominator != fraction2->denominator)
+	{
+		a = (lcm / fraction1->denominator) * fraction1->numerator;
+		b = (lcm / fraction2->denominator) * fraction2->numerator;
+	}
+	fraction_result->numerator = a + b;
+	fraction_result->denominator = lcm;
+	reduce(fraction_result);
+	convert_to_mix(fraction_result);
+	return fraction_result;
+}
 
-
-
-
-
+Fraction* subtract_fractions(Fraction* fraction1, Fraction* fraction2)
+{
+	Fraction* fraction_result = new Fraction();
+	int lcm = least_common_divisor(fraction1->denominator, fraction2->denominator);
+	int a = fraction1->numerator;
+	int b = fraction2->numerator;
+	if (fraction1->denominator != fraction2->denominator)
+	{
+		a = (lcm / fraction1->denominator) * fraction1->numerator;
+		b = (lcm / fraction2->denominator) * fraction2->numerator;
+	}
+	fraction_result->numerator = a - b;
+	fraction_result->denominator = lcm;
+	reduce(fraction_result);
+	convert_to_mix(fraction_result);
+	return fraction_result;
+}
 
 
 
@@ -98,5 +156,13 @@ void print_rectangle(Rectangle* rectangle)
 
 int main()
 {
+	Fraction* fraction = new Fraction();
+	Fraction* fraction2 = new Fraction();
+	fraction->numerator = 5;
+	fraction->denominator = 8;
+	fraction2->numerator = 10;
+	fraction2->denominator = 25;
+	Fraction* fraction3 = sum_fractions(fraction, fraction2);
+	print_fraction(fraction3);
 	
 }
