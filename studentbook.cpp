9aa6a49170da726018cd58b08349a7db656b6ccf
Seymur Bagirov <megadestroyer230@gmail.com>
2021-09-23 18:15:40 +0400

    change studentbook.cpp

--- studentbook.cpp
+++ studentbook.cpp
@@ -3,7 +3,7 @@
 struct Student_Book
 {
     char* name;
-    bool grade1 : 1; 
+    bool grade1 : 1;
     bool grade2 : 1;
     bool grade3 : 1;
     bool grade4 : 1;
@@ -13,20 +13,24 @@ struct Student_Book
     bool grade8 : 1;
     bool grade9 : 1;
     bool grade10 : 1;
+    ~Student_Book()
+    {
+        delete[] name;
+    }
 };
 
 
 union data_structure
 {
-    unsigned long var;
-    long var;
+    unsigned long ulong;
+    long longg;
 };
 
 void obj_init(Student_Book* student_book, const char* name, size_t buffer_size = 100)
 {
     student_book->name = new char[buffer_size];
     strcpy_s(student_book->name, buffer_size, name);
-    
+
 }
 
 void init_arr(Student_Book** arr, size_t size)
@@ -43,11 +47,11 @@ void init_arr(Student_Book** arr, size_t size)
 double get_average_grade(Student_Book* student_book)
 {
     double count = 0;
-    char* object = (char*) student_book;
+    char* object = (char*)student_book;
     object += sizeof(char*);
-    for (int i = 0 ; i < 10; i++)
+    for (int i = 0; i < 10; i++)
     {
-        if (*(bool*) object == true)
+        if (*(bool*)object == true)
         {
             count++;
         }
@@ -55,13 +59,13 @@ double get_average_grade(Student_Book* student_book)
     }
     return (count / 10) * 100;
 }
- 
+
 
 
 std::ostream& operator << (std::ostream& stream, Student_Book* student_book)
 {
     stream << "Student name: " << student_book->name << "\n";
-    char* object = (char*) student_book;
+    char* object = (char*)student_book;
     object += sizeof(char*);
     for (int i = 0; i < 10; i++)
     {
@@ -76,7 +80,7 @@ std::ostream& operator << (std::ostream& stream, Student_Book* student_book)
 
 void push_back(Student_Book**& student_book, Student_Book* student, size_t& size)
 {
-    Student_Book** new_student_book = new Student_Book*[size + 1];
+    Student_Book** new_student_book = new Student_Book * [size + 1];
     for (int i = 0; i < size; i++)
     {
         new_student_book[i] = student_book[i];
@@ -100,6 +104,7 @@ void give_grades(Student_Book* student_book, bool* grades)
 
 void print_students(Student_Book** student_book, const size_t size)
 {
+    std::cout << "List of students: \n";
     for (int i = 0; i < size; i++)
     {
         std::cout << i + 1 << ". " << student_book[i]->name << "\n";
@@ -107,32 +112,44 @@ void print_students(Student_Book** student_book, const size_t size)
     std::cout << "\n";
 }
 
-void print_student(Student_Book** book, char* target, const size_t size)
+void print_student(Student_Book** book, const char* target, const size_t size)
 {
     for (int i = 0; i < size; i++)
     {
         if (strcmp(book[i]->name, target) == 0)
         {
             std::cout << book[i];
-            break;
+            return;
         }
     }
+   
     std::cout << "There is no such student" << std::endl;
 }
 
-// void print_debt(Student_Book** book, const size_t size)
-// {
-//     for (int i = 0; i < size; i++)
-//     {
-
-//     }
-// }
-
-enum class TYPE
+void print_debt(Student_Book** book, const size_t size)
 {
-    ULONG,
-    LONG
-};
+    std::cout << "List of students that have debt: " << "\n";
+    for (int i = 0; i < size; i++)
+    {
+        char* temp = (char*)book[i];
+        temp += sizeof(char*);
+        int count = 0;
+        for (int j = 0; j < 10; j++)
+        {
+            if (*(bool*)temp == 0)
+            {
+                count++;
+            }
+            temp++;
+        }
+        if (count >= 5)
+        {
+            std::cout << book[i]->name << "\n";
+        }
+    }
+    std::cout << std::endl;
+}
+
 
 
 
@@ -145,11 +162,26 @@ enum class TYPE
 
 int main()
 {
-    // Student_Book* student_book = new Student_Book;
-    // init(student_book, "Seymur");
-    // std::cout << student_book << "\n";
+    //Student_Book* student_book = new Student_Book;
+    //obj_init(student_book, "Seymur");
+   // std::cout << student_book << "\n";
     // size_t size = 0;
-    // Student_Book** student_book = new Student_Book*[size];
+    // Student_Book** student_book = new Student_Book * [size];
+    // Student_Book* student = new Student_Book;
+    // obj_init(student, "xz");
+    // bool grades[10] = { 0,1,1,0,0,0,0,0,0,1 };
+    // give_grades(student, grades);
+    // push_back(student_book, student, size);
+    // print_students(student_book, size);
+    // print_student(student_book, "xz", size);
+    // std::cout << "\n";
+    // print_debt(student_book, size);
+    // delete[] student_book;
+
     
-    
+    // data_structure data;
+    // data.ulong = 32;
+    // std::cout << typeid(data.ulong).name() << "\n";
+    // data.longg = 32;
+    // std::cout << typeid(data.longg).name();
 }
